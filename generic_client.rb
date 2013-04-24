1fa278448b25fb78768d7903d609e74fb561bdbf
sonic45132 <sonicblue700@yahoo.com>
2013-04-24 15:10:03 -0400

    Added networking code to GenericClient

--- generic_client.rb
+++ generic_client.rb
@@ -5,7 +5,30 @@ class GenericClient
   def initialize
   end
 
-  def sendData(url, headers, data)
+  def send_data(uri, headers, data)
+
+    if data == nil
+      req = Net::HTTP::Get.new(uri)
+    else
+      req = Net::HTTP::Post.new(uri)
+      req.set_form_data(data)
+    end
+
+    headers.each do |key, value|
+      req[key] = value
+    end
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    case res
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        return res
+      else
+        puts res.value
+        return nil
+    end
 
   end
 
